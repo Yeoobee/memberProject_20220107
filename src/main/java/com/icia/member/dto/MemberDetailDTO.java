f8e6914fc8b416354ce92ea387d13e57638590cc
yeoobee <ppang407@gmail.com>
2022-01-07 14:43:53 +0900

    회원가입,로그인,목록,상세정보

---
+++ src/main/java/com/icia/member/dto/MemberDetailDTO.java
@@ -0,0 +1,26 @@
+package com.icia.member.dto;
+
+import com.icia.member.entity.MemberEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberDetailDTO {
+    private Long memberId;
+    private String memberEmail;
+    private String memberPassword;
+    private String memberName;
+
+    // MemberEntity -> MemberDetailDTO
+    public static MemberDetailDTO toMemberDetailDTO(MemberEntity memberEntity){
+        MemberDetailDTO memberDetailDTO = new MemberDetailDTO();
+        memberDetailDTO.setMemberId(memberEntity.getId());
+        memberDetailDTO.setMemberEmail(memberEntity.getMemberEmail());
+        memberDetailDTO.setMemberPassword(memberEntity.getMemberPassword());
+        memberDetailDTO.setMemberName(memberEntity.getMemberName());
+        return memberDetailDTO;
+    }
+}
